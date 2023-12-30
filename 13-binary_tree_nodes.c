dbaac25c065b88845339e9d023683a7ad5a884a4
CletsyMedia <Cletussam12@yahoo.com>
2023-12-30 13:01:19 +0100

    A child in a B-tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-  size_t nodes = 0;
+  size_t node_Tree = 0;
 
   if (tree)
   {
-    nodes += (tree->left || tree->right) ? 1 : 0;
-    nodes += binary_tree_nodes(tree->left);
-    nodes += binary_tree_nodes(tree->right);
+    node_Tree += (tree->left || tree->right) ? 1 : 0;
+    node_Tree += binary_tree_nodes(tree->left);
+    node_Tree += binary_tree_nodes(tree->right);
   }
-  return (nodes);
+  return (node_Tree);
 }
