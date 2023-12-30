55f776b05d519e99473254836d32f03c54e2e646
CletsyMedia <Cletussam12@yahoo.com>
2023-12-30 01:27:32 +0100

    A child in a B-tree

--- 0-main.c
+++ 0-main.c
@@ -9,21 +9,21 @@
  */
 int main(void)
 {
-    binary_tree_t *root;
-    size_t size;
+  binary_tree_t *root;
+  size_t nodes;
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
+  root = binary_tree_node(NULL, 98);
+  root->left = binary_tree_node(root, 12);
+  root->right = binary_tree_node(root, 402);
+  binary_tree_insert_right(root->left, 54);
+  binary_tree_insert_right(root, 128);
+  binary_tree_print(root);
 
-    size = binary_tree_size(root);
-    printf("Size of %d: %lu\n", root->n, size);
-    size = binary_tree_size(root->right);
-    printf("Size of %d: %lu\n", root->right->n, size);
-    size = binary_tree_size(root->left->right);
-    printf("Size of %d: %lu\n", root->left->right->n, size);
-    return (0);
+  nodes = binary_tree_nodes(root);
+  printf("Nodes in %d: %lu\n", root->n, nodes);
+  nodes = binary_tree_nodes(root->right);
+  printf("Nodes in %d: %lu\n", root->right->n, nodes);
+  nodes = binary_tree_nodes(root->left->right);
+  printf("Nodes in %d: %lu\n", root->left->right->n, nodes);
+  return (0);
 }
