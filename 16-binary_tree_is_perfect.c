da1fba5770231f8971054f92ca31ffa985b477b9
CletsyMedia <Cletussam12@yahoo.com>
2023-12-31 17:33:37 +0100

    Building the perfect B-tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,3 +47,13 @@ int is_perfect_recursive(const binary_tree_t *tree,
 
 }
 
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+
+}
