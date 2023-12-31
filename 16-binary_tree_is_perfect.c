ba322e0fdace6e272fc3d6dee08099d2d46a9882
CletsyMedia <Cletussam12@yahoo.com>
2023-12-31 17:32:35 +0100

    Building the perfect B-tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,12 +1,32 @@
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL || node->left != NULL || node->right != NULL)
-		return (0);
+#include "binary_trees.h"
 
-	return (1);
+/**
+ * is_leaf - Checks if a node is a leaf in a binary tree.
+ * @node: A pointer to the node to check.
+ *
+ * Return: 1 if the node is a leaf, 0 otherwise.
+ */
+unsigned char is_leaf(const binary_tree_t *node)
+{
+
 }
 
-size_t binary_tree_depth(const binary_tree_t *tree)
+/**
+ * depth - Measures the depth of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: The depth of the tree.
+ */
+size_t depth(const binary_tree_t *tree)
 {
-	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+
 }
+
+/**
+ * get_leaf - Finds a leaf node in a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: A pointer to a leaf node in the tree.
+ */
+const binary_tree_t *get_leaf(const binary_tree_t *tree)
+
