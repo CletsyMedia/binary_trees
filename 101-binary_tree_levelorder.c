ba26b9f39ecad3c6569d42e6027a5d80b3db36c2
CletsyMedia <Cletussam12@yahoo.com>
2024-01-02 19:58:10 +0100

    B-tree loader

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * create_node - Creates a new level order queue_t node.
+ * @node: The binary tree node for the new node to contain.
+ *
+ * Return: If an error occurs, NULL.
+ *         Otherwise, a pointer to the new node.
+ */
+levelorder_queue_t *create_node(binary_tree_t *node)
+{
+  levelorder_queue_t *newNode;
+
+  newNode = malloc(sizeof(levelorder_queue_t));
+  if (newNode == NULL)
+    return (NULL);
+
+  newNode->node = node;
+  newNode->next = NULL;
+
+  return (newNode);
+}
+
+/**
+ * free_queue - Frees a level order queue_t queue.
+ * @head: A pointer to the head of the queue.
+ */
+void free_queue(levelorder_queue_t *head)
+{
+  levelorder_queue_t *temp;
+
+  while (head != NULL)
+  {
+    temp = head->next;
+    free(head);
+    head = temp;
+  }
+}
+
