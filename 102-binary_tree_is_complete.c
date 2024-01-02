bf48e7d0445d000eb4365f598151765fdb9fcc97
CletsyMedia <Cletussam12@yahoo.com>
2024-01-02 20:22:58 +0100

    Checking if B-tree is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -39,3 +39,34 @@ void free_queue(levelorder_queue_t *head)
 	head = temp;
 	}
 }
+
+
+/**
+ * push - Enqueues a binary tree node into a level-order traversal queue.
+ *
+ * @node: Pointer to the binary tree node to be enqueued.
+ * @head: Pointer to the head of the level-order queue.
+ * @tail: Double pointer to the tail of the level-order queue.
+ *
+ * This function creates a new node for the queue, appends it to the end,
+ * and updates the tail pointer accordingly.
+ */
+void push(binary_tree_t *node, levelorder_queue_t *head,
+          levelorder_queue_t **tail)
+{
+    levelorder_queue_t *newNode;
+
+    /* Create a new node for the level-order queue */
+    newNode = create_node(node);
+
+    /* Handle memory allocation failure */
+    if (newNode == NULL)
+    {
+        free_queue(head);
+        exit(1);
+    }
+
+    /* Append the new node to the end of the queue */
+    (*tail)->next = newNode;
+    *tail = newNode;
+}
