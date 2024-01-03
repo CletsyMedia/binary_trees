cc211c97bcb85b6e0917c00edf0925fdcf91c10f
CletsyMedia <Cletussam12@yahoo.com>
2024-01-03 14:49:05 +0100

    Balancing the b-tree]

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,26 +18,3 @@ int binary_tree_balance(const binary_tree_t *tree)
 	*/
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
-
-/**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
- *
- * Return: If tree is NULL, return 0, else return the height.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	/* Check if the tree is NULL */
-	if (!tree)
-	return (0);
-
-	/**
-	* Recursively calculate the height of the tree by comparing the
-	* heights of the left and right subtrees and returning the greater
-	* height incremented by 1.
-	*/
-	size_t left = binary_tree_height(tree->left);
-	size_t right = binary_tree_height(tree->right);
-
-	return ((left > right) ? left + 1 : right + 1);
-}
