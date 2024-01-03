b6e1061480b84532410323a8195bc7b39346423d
CletsyMedia <Cletussam12@yahoo.com>
2024-01-03 20:33:19 +0100

    Inserts a value in an AVL Tree

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Calculates the height of a binary tree.
+ * height - Calculates the height of a binary tree.
  * @tree: A pointer to the root node of the tree.
  *
  * Return: The height of the binary tree.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t height(const binary_tree_t *tree)
 {
 	/* Guard clause to handle NULL trees */
-	if (tree)
+	if (tree != NULL)
 	{
 	size_t len_h = 0, rig_h = 0;
 	/* Calculate the height of the left and right subtrees */
