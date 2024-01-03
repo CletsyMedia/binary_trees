000ed83e9d60421928f940feb9d6d102c8a894d9
CletsyMedia <Cletussam12@yahoo.com>
2024-01-03 03:40:23 +0100

    Search for a val in B-tree

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -6,18 +6,19 @@
  * @tree: Pointer to the root node of the BST to search.
  * @value: The value to search for in the tree.
  *
- * Return: Pointer to the node containing the specified value, or NULL if not found.
+ * Return: Pointer to the node containing the specified value,
+ * or NULL if not found.
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-    /* Check if tree is NULL or if the value is found in the current node */
-    if (tree == NULL || tree->n == value)
-        return ((bst_t *)tree);
+	/* Check if tree is NULL or if the value is found in the current node */
+	if (tree == NULL || tree->n == value)
+	return ((bst_t *)tree);
 
-    /* If the value is less than the current node's value, search in the left subtree */
-    if (value < tree->n)
-        return (bst_search(tree->left, value));
-    /* If the value is greater than the current node's value, search in the right subtree */
-    else
-        return (bst_search(tree->right, value));
+	/* If the value is < the current node's value, search in the left subtree */
+	if (value < tree->n)
+	return (bst_search(tree->left, value));
+	/* If the value is > the current node's value, search in the right subtree */
+	else
+	return (bst_search(tree->right, value));
 }
