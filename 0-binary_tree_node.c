04367646bcbbab1cd12b15e64f2e5e9ac8b21338
CletsyMedia <Cletussam12@yahoo.com>
2024-01-03 14:16:32 +0100

    Creating B-tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,13 +17,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-	if (newNode == NULL)
-		return (NULL);
-
+	if (newNode)
+	{
 	newNode->n = value;
 	newNode->parent = parent;
-	newNode->left = NULL;
-	newNode->right = NULL;
+	newNode->left = newNode->right = NULL;
+	}
 
 	return (newNode);
 }
