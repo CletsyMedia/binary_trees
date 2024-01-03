b052f279e515050f248733b54c661327d75c8bf7
CletsyMedia <Cletussam12@yahoo.com>
2024-01-03 15:39:44 +0100

    B-tree BST insert

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,86 +1,50 @@
 #include "binary_trees.h"
-#include <limits.h>
 
 /**
- * is_bst_helper - Checks if a binary tree is a valid binary search tree.
- * @tree: A pointer to the root node of the tree to check.
- * @lo: The value of the smallest node visited thus far.
- * @hi: The value of the largest node visited this far.
- *
- * Return: If the tree is a valid BST, 1, otherwise, 0.
- */
-int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
-{
-	if (tree != NULL)
-	{
-	if (tree->n < lo || tree->n > hi)
-	return (0);
-	return (is_bst_helper(tree->left, lo, tree->n - 1) &&
-	is_bst_helper(tree->right, tree->n + 1, hi));
-	}
-	return (1);
-}
-
-/**
- * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 1 if tree is a valid BST, and 0 otherwise
- */
-int binary_tree_is_bst(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-	return (0);
-	return (is_bst_helper(tree, INT_MIN, INT_MAX));
-}
-
-/**
- * bst_insert - Inserts a value into a Binary Search Tree (BST).
- *
- * @tree: Double pointer to the root node of the BST to insert the value.
+ * bst_insert - Inserts a value in a Binary Search Tree.
+ * @tree: A double pointer to the root node of the BST to insert the value.
  * @value: The value to store in the node to be inserted.
  *
- * Return: Pointer to the created node, or NULL on failure.
+ * Return: A pointer to the created node, or NULL on failure.
  */
 bst_t *bst_insert(bst_t **tree, int value)
-{ /* Check if the current node is NULL (empty subtree) */
-	if (*tree == NULL)
+{
+	/* Check if the double pointer to the root node is not NULL */
+	if (tree != NULL)
 	{
-	/* Create a new node and make it the root if the tree is empty */
-	*tree = binary_tree_node(NULL, value);
-	return (*tree);
+	bst_t *curr = *tree; /* Initialize current pointer */
+
+	/* Check if the current node is NULL, create a new node as the root */
+	if (curr == NULL)
+	{
+	bst_t *new = binary_tree_node(curr, value);
+
+	return (new == NULL ? NULL : (*tree = new));
 	}
-	/* Check if the tree is a valid BST */
-	if (!binary_tree_is_bst(*tree))
-	return (NULL);
-	/* Check if the value is less than the current node's value */
-	if (value < (*tree)->n)
+
+	/* Insert in the left subtree if the value is < the current node's value */
+	if (value < curr->n)
 	{
-	/* Recursively insert into the left subtree */
-	if ((*tree)->left == NULL)
-	{
-	(*tree)->left = binary_tree_node(*tree, value);
-	return ((*tree)->left);
+	if (curr->left != NULL)
+	return (bst_insert(&curr->left, value));
+
+	bst_t *new = binary_tree_node(curr, value);
+
+	return (new == NULL ? NULL : (curr->left = new));
 	}
-	else
+
+	/* Insert in the right subtree if the value is > the current node's value */
+	if (value > curr->n)
 	{
-	return (bst_insert(&((*tree)->left), value));
-	}
-	}
-	/* Check if the value is greater than the current node's value */
-	else if (value > (*tree)->n)
-	{
-	/* Recursively insert into the right subtree */
-	if ((*tree)->right == NULL)
-	{
-	(*tree)->right = binary_tree_node(*tree, value);
-	return ((*tree)->right);
-	}
-	else
-	{
-	return (bst_insert(&((*tree)->right), value));
+	if (curr->right != NULL)
+	return (bst_insert(&curr->right, value));
+
+	bst_t *new = binary_tree_node(curr, value);
+
+	return (new == NULL ? NULL : (curr->right = new));
 	}
 	}
-	/* If the value is equal, ignore and return NULL (already present) */
+
+	/* Return NULL if the tree or value is NULL */
 	return (NULL);
 }
