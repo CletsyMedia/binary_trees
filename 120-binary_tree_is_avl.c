8ee94a16568fb72423fba0245b7a3fe82ff337e2
CletsyMedia <Cletussam12@yahoo.com>
2024-01-03 16:38:48 +0100

    Checks if a B-tree is valid

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -2,20 +2,20 @@
 #include "limits.h"
 
 /**
- * height - Computes the height of a binary tree.
+ * height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
  *
  * Return: If tree is NULL, your function must return 0, else return height.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-	size_t leftH = 0, rightH = 0;
+	size_t l_height = 0, r_height = 0;
 
-	leftH = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-	rightH = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-	return ((leftH > rightH) ? leftH : rightH);
+	l_height = tree->left ? 1 + height(tree->left) : 1;
+	r_height = tree->right ? 1 + height(tree->right) : 1;
+	return ((l_height > r_height) ? l_height : r_height);
 	}
 	return (0);
 }
