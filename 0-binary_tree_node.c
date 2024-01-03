e613d0d93c86e25fcc952b7b3aa202d2f459a96d
CletsyMedia <Cletussam12@yahoo.com>
2024-01-03 14:14:18 +0100

    Creating tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,11 +18,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
 	if (newNode == NULL)
-	return (NULL);
+		return (NULL);
 
-	*newNode = (binary_tree_t){.n = value, .parent = parent};
-	return (newNode);
-
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
 	return (newNode);
 }
