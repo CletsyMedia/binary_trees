1fd67a27e11b3ffef6171b32094525d9dda559fa
CletsyMedia <Cletussam12@yahoo.com>
2024-01-03 14:33:56 +0100

    Insert B-tree right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,16 +15,16 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (!parent)
+	binary_tree_t *newNode;
 
+	if (parent == NULL)
 	return (NULL);
 
-	binary_tree_t *newNode = binary_tree_node(parent, value);
-
-	if (!newNode)
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
 	return (NULL);
 
-	if (parent->right)
+	if (parent->right != NULL)
 	{
 	newNode->right = parent->right;
 	parent->right->parent = newNode;
