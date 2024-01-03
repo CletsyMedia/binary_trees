0205349b687849a16ef73feec4150081c2bcaac1
CletsyMedia <Cletussam12@yahoo.com>
2024-01-03 16:34:44 +0100

    Checks if a B-tree is valid

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -2,48 +2,51 @@
 #include "limits.h"
 
 /**
- * height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
+ * calculate_height - Computes the height of a binary tree.
+ * @tree: A pointer to the root node of the tree.
  *
- * Return: If tree is NULL, your function must return 0, else return height.
+ * Return: The height of the tree or 0 if the tree is empty.
  */
-size_t height(const binary_tree_t *tree)
+size_t calculate_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-	size_t l = 0, r = 0;
+	size_t left_height = tree->left ? 1 + calculate_height(tree->left) : 1;
+	size_t right_height = tree->right ? 1 + calculate_height(tree->right) : 1;
 
-	l = tree->left ? 1 + height(tree->left) : 1;
-	r = tree->right ? 1 + height(tree->right) : 1;
-	return ((l > r) ? l : r);
+	return (left_height > right_height ? left_height : right_height);
 	}
 	return (0);
 }
 
 /**
- * is_avl_helper - Checks if a binary tree is a valid AVL tree.
- * @tree: A pointer to the root node of the tree to check.
- * @lo: The value of the smallest node visited thus far.
- * @hi: The value of the largest node visited this far.
+ * is_avl_tree - Checks if a binary tree is a valid AVL tree.
+ * @tree: A pointer to the root node of the tree.
+ * @min: The minimum value allowed for a node.
+ * @max: The maximum value allowed for a node.
  *
- * Return: If the tree is a valid AVL tree, 1, otherwise, 0.
+ * Return: 1 if the tree is a valid AVL tree, and 0 otherwise.
  */
-int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
+int is_avl_tree(const binary_tree_t *tree, int min, int max)
 {
-	size_t lhgt, rhgt, diff;
+	size_t left_h, right_hei, height_diff;
 
-	if (tree != NULL)
+	if (tree)
 	{
-	if (tree->n < lo || tree->n > hi)
+	if (tree->n < min || tree->n > max)
 	return (0);
-	lhgt = height(tree->left);
-	rhgt = height(tree->right);
-	diff = lhgt > rhgt ? lhgt - rhgt : rhgt - lhgt;
-	if (diff > 1)
+
+	left_h = calculate_height(tree->left);
+	right_hei = calculate_height(tree->right);
+	height_diff = left_h > right_hei ? left_h - right_hei : right_hei - left_h;
+
+	if (height_diff > 1)
 	return (0);
-	return (is_avl_helper(tree->left, lo, tree->n - 1) &&
-	is_avl_helper(tree->right, tree->n + 1, hi));
+
+	return (is_avl_tree(tree->left, min, tree->n - 1) &&
+	is_avl_tree(tree->right, tree->n + 1, max));
 	}
+
 	return (1);
 }
 
